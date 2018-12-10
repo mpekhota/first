88acad5e77dcbc6bfcc09180771cfb9ad76acae1
Mikhail Pekhota <43859010+mpekhota@users.noreply.github.com>
2018-12-10 03:05:45 +0300

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,7 +8,29 @@ docker_service 'default' do
     action [:create, :start]
 end
 
-docker_image "#{node['bluegreen']['url']}/bluegreen" do
-    tag node['bluegreen']['version']
+docker_image "#{node['registry']['url']}/first" do
+    tag node['registry']['version']
     action :pull
+end
+
+isBlueRunning = `docker ps | grep '8080->' -c`
+
+if isBlueRunning == 0
+    docker_container 'nodeBlue' do
+        repo "#{node['registry']['url']}/first"
+        tag node['registry']['version']
+        port '8080:8080'
+    end	
+    docker_container 'nodeGreen' do
+        action [:kill, :delete]
+    end
+else
+    docker_container 'nodeGreen' do
+        repo "#{node['registry']['url']}/first"
+        tag node['registry']['version']
+        port '8081:8080'
+    end 
+    docker_container 'nodeBlue' do
+        action [:kill, :delete]
+    end
 end

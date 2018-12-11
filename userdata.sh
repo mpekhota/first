9af4e38d696225786fcc73b59554255bd3a906c7
Mikhail Pekhota <43859010+mpekhota@users.noreply.github.com>
2018-12-12 00:11:55 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-yum install httpd mc epel-release net-tool htop -y
+yum install httpd -y
 echo This page running in Amazon Cloud! > /var/www/html/index.html
 touch /var/www/html/index.html && chmod 0755 /var/www/html/index.html
 echo This page running in Amazon Cloud! > /var/www/html/index.html
 service httpd start
-chkconfig httpd on

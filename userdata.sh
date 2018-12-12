15ed87c0e86b9e6f0e986cea97fb8626912db67f
Mikhail Pekhota <43859010+mpekhota@users.noreply.github.com>
2018-12-12 13:19:17 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 yum install httpd -y
-echo This page running in Amazon Cloud! > /var/www/html/index.html
 touch /var/www/html/index.html && chmod 0755 /var/www/html/index.html
 echo This page running in Amazon Cloud! > /var/www/html/index.html
 service httpd start
